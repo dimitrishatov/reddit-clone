89b9aaf8a98ff59f43d32772c3e23653bf18d8a8
Dimitri Shatov <dimitri.113322@gmail.com>
2021-06-23 17:49:06 -0500

    Added Post entity

---
+++ src/entities/Post.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core"; 
+
+// Entity corresponds to a db table
+@Entity()
+export class Post {
+   // These correspond to columns in our db table
+   @PrimaryKey()
+   id!: number; 
+
+   @Property()
+   createdAt = new Date(); 
+
+   @Property({ onUpdate: () => new Date() })
+   updatedAt = new Date();
+
+   @Property()
+   title!: string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
+import { Post } from "./entities/Post"
 
 const main = async () => {
    // This interacts with database and returns a 
    // promise so we await it
    const orm = await MikroORM.init({
+      entities: [Post],
       dbName: 'reddit',
       type: 'postgresql', 
       debug: !__prod__
