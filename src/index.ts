b5c9f992a703e93f45c68f1b8b7ff003a18b5a0b
Dimitri Shatov <dimitri.113322@gmail.com>
2021-06-24 14:40:35 -0500

    migration automatically runs in code, better error messages

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,15 @@ import microConfig from "./mikro-orm.config"
 const main = async () => {
    const orm = await MikroORM.init(microConfig);
 
+   // Automatically run migration in code
+   await orm.getMigrator().up();
+
    const post = orm.em.create(Post, { title: "my first post" });
    await orm.em.persistAndFlush(post);
-   console.log('sql 2'); 
-   await orm.em.nativeInsert(Post, { title: "my first post 2"}); 
-   }); 
-}
+}; 
 
-main()
+main().catch((err) => {
+   console.error(err);
+});
 
 console.log("hello world")
