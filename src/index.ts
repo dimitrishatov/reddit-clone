03cc35a5556340fc72434ec175457a0dd39b5070
Dimitri Shatov <dimitri.113322@gmail.com>
2021-06-24 14:48:46 -0500

    finished basic mikroorm setup

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@ const main = async () => {
 
    const post = orm.em.create(Post, { title: "my first post" });
    await orm.em.persistAndFlush(post);
+
+   const posts = await orm.em.find(Post, {});
+   console.log(posts);
 }; 
 
 main().catch((err) => {
