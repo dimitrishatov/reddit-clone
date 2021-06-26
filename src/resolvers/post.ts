edb01d78cacb79c26b30e29c8eac672134c9ecf9
Dimitri Shatov <dimitri.113322@gmail.com>
2021-06-26 13:42:44 -0500

    added resolver for viewing single post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,12 +1,22 @@
-import { Resolver, Query, Ctx } from "type-graphql"
+import { Resolver, Query, Ctx, Arg, Int } from "type-graphql"
 import { Post } from "../entities/Post"; 
 import { MyContext } from "../types"
 
 @Resolver()
 export class PostResolver {
+   // READ ALL POSTS
    @Query(() => [Post]) //Graphql type
    // {em} instead of ctx for destructuring
    posts(@Ctx() {em}: MyContext): Promise<Post[]> { //ts type
       return em.find(Post, {}); 
+   }
+
+   // READ SINGLE POST BY ID
+   @Query(() => Post, {nullable: true}) 
+   post(
+      @Arg('id', () => Int) id: number, 
+      @Ctx() {em}: MyContext
+   ): Promise<Post | null> {
+      return em.findOne(Post, { id });
    }
 }
