58e5026b7be436b9e1c9a3b455d52feacacfa41a
Dimitri Shatov <dimitri.113322@gmail.com>
2021-06-26 14:22:59 -0500

    added user entity to begin work on authentication

---
+++ src/entities/User.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core"; 
+import { ObjectType, Field } from "type-graphql"
+
+// Entity corresponds to a db table
+@ObjectType() // Makes this GraphQL Type
+@Entity()
+export class User {
+   // Properties  correspond to columns in our db table
+   @Field()
+   @PrimaryKey()
+   id!: number; 
+
+   @Field(() => String)
+   @Property({ type: "date"  })
+   createdAt = new Date(); 
+
+   @Field(() => String)
+   @Property({ type: "date",  onUpdate: () => new Date() })
+   updatedAt = new Date();
+
+   @Field()
+   @Property({ type: "text", unique: true})
+   username!: string;
+
+   @Property({ type: "text"})
+   password!: string;
+}
