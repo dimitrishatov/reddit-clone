213dcbe917076449e9f307b070913e9ed43c2e8a
Dimitri Shatov <dimitri.113322@gmail.com>
2021-06-28 19:25:31 -0500

    added missing import

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import { UserResolver } from "./resolvers/user";
 import redis from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis'
+import { MyContext } from './types'
 
 const main = async () => {
    const orm = await MikroORM.init(microConfig);
