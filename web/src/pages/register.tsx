b0fb240a1067f8d3eff2dedc5dca193e76071fa8
Dimitri Shatov <dimitri.113322@gmail.com>
2021-07-02 18:17:59 -0500

    graphql codegen hooks now being used, basic error message setup

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -3,34 +3,27 @@ import { Formik, Form } from 'formik'
 import { Wrapper } from '../components/Wrapper'
 import { InputField } from '../components/InputField'
 import { Box, Button } from '@chakra-ui/react'
-import { useMutation } from 'urql'
+import { useRegisterMutation } from "../generated/graphql" 
 
 interface registerProps {}
 
-const REGISTER_MUT = `
-mutation Register($username: String!, $password: String!) {
-  register(options: { username: $username, password: $password }) {
-    errors {
-      field
-      message
-    }
-    user {
-      id
-      username
-    }
-  }
-}
-`
-
 export const Register: React.FC<registerProps> = ({}) => {
-	const [,register] = useMutation(REGISTER_MUT); 
+	const [,register] = useRegisterMutation(); 
 	return (
 		<Wrapper variant="small">
 			<Formik 
 				initialValues={{username: '', password: ''}}
-				onSubmit={(values) => {
+				onSubmit={ async (values, {setErrors}) => {
 					console.log(values)
-					return register(values)
+
+					const response = await register(values); 
+					// optional chaining returns undefined if no data
+					if(response.data?.register.errors) {
+						setErrors({
+							username: "hey im an error"
+						})
+					}
+					
 				}}
 			>
 				{({ isSubmitting }) => (
