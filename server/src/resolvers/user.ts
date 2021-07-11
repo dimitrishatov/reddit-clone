3102ccf2c48721cdb5790f95eb4e5816b57ba9c6
Dimitri Shatov <dimitri.113322@gmail.com>
2021-07-10 23:19:39 -0500

    adding token and user id to redis for check

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,64 +1,78 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Query, Resolver, Mutation, Arg, Ctx, Field, ObjectType } from "type-graphql";
-import argon2 from "argon2"
-import { EntityManager } from '@mikro-orm/postgresql'
-import { COOKIE_NAME } from "../constants";
+import {
+	Query,
+	Resolver,
+	Mutation,
+	Arg,
+	Ctx,
+	Field,
+	ObjectType,
+} from "type-graphql";
+import argon2 from "argon2";
+import { EntityManager } from "@mikro-orm/postgresql";
+import { COOKIE_NAME, FORGET_PASSWORD_PREFIX } from "../constants";
 import { validateRegister } from "../utils/validateRegister";
 import { UsernamePasswordInput } from "./UsernamePasswordInput";
 import { sendEmail } from "../utils/sendEmail";
-import {v4} from 'uuid'
+import { v4 } from "uuid";
 
-@ObjectType() 
+@ObjectType()
 class FieldError {
 	@Field()
-	field: string
+	field: string;
 
 	@Field()
-	message: string
+	message: string;
 }
 
-@ObjectType() 
+@ObjectType()
 class UserResponse {
-	@Field(() => [FieldError], {nullable: true})
-	errors?: FieldError[]
+	@Field(() => [FieldError], { nullable: true })
+	errors?: FieldError[];
 
-	@Field(() => User, {nullable: true})
-	user?: User
+	@Field(() => User, { nullable: true })
+	user?: User;
 }
 
 @Resolver()
 export class UserResolver {
 	@Mutation(() => Boolean)
 	async forgotPassword(
-		@Arg('email') email: string,
-		@Ctx() { em } : MyContext
+		@Arg("email") email: string,
+		@Ctx() { em, redis }: MyContext
 	) {
-		const user = await em.findOne(User, {email})
+		const user = await em.findOne(User, { email });
 		if (!user) {
 			// the email is not in the database
 			return true;
 		}
 
-		// random string for now
-		const token = v4()
-		
-		await sendEmail(
-			email, 
-			`<a href="http://localhost:3000/change-password/${token}>reset password</a>`
-		)
+		const token = v4();
 
-		return true; 
+		await redis.set(
+			FORGET_PASSWORD_PREFIX + token,
+			user.id,
+			"ex", //expires
+			1000 * 60 * 60 * 24 * 3
+		);
+
+		await sendEmail(
+			email,
+			`<a href="http://localhost:3000/change-password/${token}>reset password</a>`
+		);
+
+		return true;
 	}
 
-	@Query(() => User, {nullable: true})
+	@Query(() => User, { nullable: true })
 	async me(@Ctx() { req, em }: MyContext) {
 		// you are not logged in (otherwise login resolver wouldve set this)
 		if (!req.session.userId) {
-			return null
+			return null;
 		}
-		const user = await em.findOne(User, {id: req.session.userId})
-		return user
+		const user = await em.findOne(User, { id: req.session.userId });
+		return user;
 	}
 
 	@Mutation(() => UserResponse)
@@ -66,12 +80,12 @@ export class UserResolver {
 		@Arg("options") options: UsernamePasswordInput,
 		@Ctx() { em, req }: MyContext
 	): Promise<UserResponse> {
-		const errors = validateRegister(options); 
+		const errors = validateRegister(options);
 		if (errors) {
-			return { errors }
+			return { errors };
 		}
 
-		const hashedPassword = await argon2.hash(options.password)
+		const hashedPassword = await argon2.hash(options.password);
 		let user;
 		try {
 			const result = await (em as EntityManager)
@@ -82,27 +96,27 @@ export class UserResolver {
 					email: options.email,
 					password: hashedPassword,
 					created_at: new Date(),
-					updated_at: new Date()
-			})
-				.returning("*");;
-			user = result[0]; 
+					updated_at: new Date(),
+				})
+				.returning("*");
+			user = result[0];
 		} catch (err) {
 			// duplicate username error
-			if (err.code === '23505' || err.detail.includes("already exists")) {
+			if (err.code === "23505" || err.detail.includes("already exists")) {
 				return {
 					errors: [
 						{
-							field: "username", 
-							message: "username already taken"
-						}
-					]
-				}
+							field: "username",
+							message: "username already taken",
+						},
+					],
+				};
 			}
-			console.log("message: ", err.message)
-		} 
+			console.log("message: ", err.message);
+		}
 
 		// store user id in session
-		req.session.userId = user.id
+		req.session.userId = user.id;
 
 		return { user };
 	}
@@ -110,55 +124,58 @@ export class UserResolver {
 	@Mutation(() => UserResponse)
 	async login(
 		@Arg("usernameOrEmail") usernameOrEmail: string,
-		@Arg("password") password: string, 
+		@Arg("password") password: string,
 		@Ctx() { em, req }: MyContext
 	): Promise<UserResponse> {
 		const user = await em.findOne(
 			User,
-			usernameOrEmail.includes('@') 
-			? { email: usernameOrEmail } 
-			: { username: usernameOrEmail }
+			usernameOrEmail.includes("@")
+				? { email: usernameOrEmail }
+				: { username: usernameOrEmail }
 		);
 		if (!user) {
 			return {
-				errors: [{
-					field: "usernameOrEmail", 
-					message: "that username or email doesn't exist"
-				}]
-			}
+				errors: [
+					{
+						field: "usernameOrEmail",
+						message: "that username or email doesn't exist",
+					},
+				],
+			};
 		}
-		const valid = await argon2.verify(user.password, password)
+		const valid = await argon2.verify(user.password, password);
 		if (!valid) {
 			return {
-				errors: [{
-					field: "password", 
-					message: "incorrect password"
-				}]
-			}
+				errors: [
+					{
+						field: "password",
+						message: "incorrect password",
+					},
+				],
+			};
 		}
 
 		// lets us know who the current user is
-		req.session.userId = user.id; 
+		req.session.userId = user.id;
 
 		return {
 			user,
-		}
+		};
 	}
 
 	// remove session in redis and clear cookie
 	@Mutation(() => Boolean)
 	logout(@Ctx() { req, res }: MyContext) {
-		return new Promise((resolve) => 
+		return new Promise((resolve) =>
 			req.session.destroy((err: any) => {
-				res.clearCookie(COOKIE_NAME)
+				res.clearCookie(COOKIE_NAME);
 				if (err) {
-					console.log(err)
-					resolve(false)
-					return
+					console.log(err);
+					resolve(false);
+					return;
 				}
-				resolve(true)
+				resolve(true);
 			})
-		)
+		);
 	}
 }
-
