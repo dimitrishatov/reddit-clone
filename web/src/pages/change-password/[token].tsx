b5b36b27f2276fa9bb9a08ccc14d08f5e68f8a4d
Dimitri Shatov <dimitri.113322@gmail.com>
2021-07-10 23:47:45 -0500

    added basic frontend

--- web/src/pages/change-password/[token].tsx
+++ web/src/pages/change-password/[token].tsx
@@ -1,7 +1,48 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
 import { NextPage } from "next";
+import React from "react";
+import { InputField } from "../../components/InputField";
+import { Wrapper } from "../../components/Wrapper";
 
 export const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
-	return <div>token is: {token}</div>;
+	return (
+		<Wrapper variant="small">
+			<Formik
+				initialValues={{ newPassword: "" }}
+				onSubmit={async (values, { setErrors }) => {
+					// const response = await login(values);
+					// // optional chaining returns undefined if no data
+					// if (response.data?.login.errors) {
+					// 	// ui will show graphql error messages
+					// 	setErrors(toErrorMap(response.data.login.errors));
+					// } else if (response.data?.login.user) {
+					// 	// worked
+					// 	router.push("/");
+					// }
+				}}
+			>
+				{({ isSubmitting }) => (
+					<Form>
+						<InputField
+							name="newPassword"
+							placeholder="new password"
+							label="New Password"
+							type="password"
+						/>
+						<Button
+							mt={4}
+							type="submit"
+							isLoading={isSubmitting}
+							colorScheme="teal"
+						>
+							change password
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</Wrapper>
+	);
 };
 
 ChangePassword.getInitialProps = ({ query }) => {
