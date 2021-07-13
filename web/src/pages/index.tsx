d3d4a8ecd99c6446f56209ff66cda0f32fe8a834
Dimitri Shatov <dimitri.113322@gmail.com>
2021-07-13 16:57:52 -0500

    added create post link to index

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,22 +1,25 @@
-import { NavBar } from "../components/NavBar"
-import { createUrqlClient } from "../utils/createUrqlClient"
-import { withUrqlClient } from 'next-urql'
-import { usePostsQuery } from "../generated/graphql"
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { withUrqlClient } from "next-urql";
+import { usePostsQuery } from "../generated/graphql";
+import { Layout } from "../components/Layout";
+import NextLink from "next/link";
+import { Link } from "@chakra-ui/react";
 
 const Index = () => {
-  const [{data}] = usePostsQuery()
+  const [{ data }] = usePostsQuery();
   return (
-      <>
-        <NavBar/>
-        <div>Hello world</div>
-        <br/>
-        {!data ? (
-          <div>loading...</div>
-        ) : (
-          data.posts.map((p) => <div key={p.id}>{p.title}</div>)
-        )}
-      </>  
-    )
-}
+    <Layout>
+      <NextLink href="/create-post">
+        <Link>create post</Link>
+      </NextLink>
 
-export default withUrqlClient(createUrqlClient, {ssr: true})(Index)
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </Layout>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
