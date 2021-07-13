05f5641bb33197f6a4433b8a90817ca409a1a656
Dimitri Shatov <dimitri.113322@gmail.com>
2021-07-13 16:47:13 -0500

    added effect to take user to login page if they load create-post and arent logged in

--- web/src/pages/create-post.tsx
+++ web/src/pages/create-post.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Formik, Form } from "formik";
 import { InputField } from "../components/InputField";
 import { Box, Button } from "@chakra-ui/react";
-import { useCreatePostMutation } from "../generated/graphql";
+import { useCreatePostMutation, useMeQuery } from "../generated/graphql";
 import { useRouter } from "next/router";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { Layout } from "../components/Layout";
 
 const CreatePost: React.FC<{}> = ({}) => {
+	const [{ data, fetching }] = useMeQuery();
 	const router = useRouter();
 	const [, createPost] = useCreatePostMutation();
+
+	useEffect(() => {
+		if (!fetching && !data?.me) {
+			router.replace("/login");
+		}
+	}, [fetching, data, router]);
+
 	return (
 		<Layout variant="small">
 			<Formik
