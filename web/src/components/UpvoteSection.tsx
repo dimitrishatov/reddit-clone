26fc3fbeb4201fb11d1b6b7c951d32474f5c6ea3
Dimitri Shatov <dimitri.113322@gmail.com>
2021-07-21 22:20:29 -0500

    buttons were so ugly had to push fix to master

--- web/src/components/UpvoteSection.tsx
+++ web/src/components/UpvoteSection.tsx
@@ -16,10 +16,11 @@ export const UpvoteSection: React.FC<UpvoteSectionProps> = ({ post }) => {
 		<Flex direction="column" justifyContent="center" alignItems="center">
 			<IconButton
 				aria-label="upvote"
+				fontSize={post.voteStatus === 1 ? "45px" : undefined}
 				icon={<ChevronUpIcon size="24px" />}
-				colorScheme={post.voteStatus === 1 ? "green" : undefined}
+				colorScheme={post.voteStatus === 1 ? "teal" : undefined}
 				isLoading={loadingState === "upvote-loading"}
-				variant={post.voteStatus === 1 ? "outline" : undefined}
+				variant={post.voteStatus === 1 ? "ghost" : undefined}
 				onClick={async () => {
 					if (post.voteStatus === 1) {
 						return;
@@ -35,9 +36,10 @@ export const UpvoteSection: React.FC<UpvoteSectionProps> = ({ post }) => {
 			{post.points}
 			<IconButton
 				aria-label="downvote"
+				fontSize={post.voteStatus === -1 ? "45px" : undefined}
 				icon={<ChevronDownIcon size="24px" />}
 				isLoading={loadingState === "downvote-loading"}
-				variant={post.voteStatus === -1 ? "outline" : undefined}
+				variant={post.voteStatus === -1 ? "ghost" : undefined}
 				colorScheme={post.voteStatus === -1 ? "red" : undefined}
 				onClick={async () => {
 					if (post.voteStatus === -1) {
